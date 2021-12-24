32936118e51e62c2c694c3fda3e206cea72ab2ad
Michael <michaeljohannpeter@gmail.com>
2021-12-24 14:40:10 +0100

    typo

--- local/bin/change-git-author.sh
+++ local/bin/change-git-author.sh
@@ -14,7 +14,7 @@ EOF
   echo "Continue to git filter-repo? DO THIS ONLY IF YOU KNOW WHAT YOU ARE DOING!"
   select choice in "Yes" "No"; do
     case $choice in
-      Yes ) git filter-repo --force --partial --mailmap my-mailmap; rm my-mailmap; echo "run git fetch && git push --foce-with-lease"; break;;
+      Yes ) git filter-repo --force --partial --mailmap my-mailmap; rm my-mailmap; echo "run git fetch && git push --force-with-lease"; break;;
       No ) rm my-mailmap; exit;;
     esac
   done
