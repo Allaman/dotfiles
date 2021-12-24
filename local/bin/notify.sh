30981e1529085cb42d425e82df8e51ee32ea0c9a
Michael <michaeljohannpeter@gmail.com>
2021-12-24 14:40:22 +0100

    notify: add darwin check

--- local/bin/notify.sh
+++ local/bin/notify.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 function notify {
-	if [[ -z "${DISPLAY// }" ]]
-	then
-		# no x server detected
-		echo "$1"
-	elif ! [[ -z "${DISPLAY// }" ]]
-	then
-		# x server detected
-		notify-send "$1"
-	else
-		# error
-		echo "Cannot check for running x server"
-		exit 1
-	fi
+  if [[ "$OSTYPE" == "darwin"* ]]
+    then
+      osascript -e "display notification \"$1\" with title \"Notify\""
+    else
+  	if [[ -z "${DISPLAY// }" ]]
+  	then
+  		# no x server detected
+  		echo "$1"
+  	elif ! [[ -z "${DISPLAY// }" ]]
+  	then
+  		# x server detected
+  		notify-send "$1"
+  	else
+  		# error
+  		echo "Cannot check for running x server"
+  		exit 1
+  	fi
+  fi
 }
