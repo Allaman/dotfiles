5962e0a9ece97037f7a515789ce34d094f5d2d5b
Michael <michaeljohannpeter@gmail.com>
2022-02-12 13:01:32 +0100

    add format.sh for formatted text functions

---
+++ local/bin/format.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function set_bold {
+  tput bold
+}
+function set_undelined {
+  tput smul
+}
+function set_reversed {
+  tput rev
+}
+function set_magenta {
+  tput setaf 5
+}
+function set_read {
+  tput setaf 1
+}
+function set_cyan {
+  tput setaf 6
+}
+function set_yellow {
+  tput setaf 3
+}
+function set_green {
+  tput setaf 2
+}
+function set_blue {
+  tput setaf 4
+}
+function set_norm {
+  tput sgr0
+}
+
+
