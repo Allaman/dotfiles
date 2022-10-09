bd299587be2d74c879a686c901d7641ab4ea9339
Michael <michaeljohannpeter@gmail.com>
2022-10-09 20:33:29 +0200

    tmux-sessionizer: fix quoting

--- local/bin/tmux-sessionizer.sh
+++ local/bin/tmux-sessionizer.sh
@@ -19,12 +19,12 @@ selected_name="$(basename "$selected" | tr . _)"
 tmux_running="$(pgrep tmux)"
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
+    tmux new-session -s "$selected_name "-c "$selected"
     exit 0
 fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
+if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
-tmux switch-client -t $selected_name
+tmux switch-client -t "$selected_name"
